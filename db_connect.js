15fb6716324f4582b4bcb068ee82df9f562ca924
cherrycharan238 <cherryiiit1234@gmail.com>
2024-09-16 21:31:47 +0530

    atlas uri

--- db_connect.js
+++ db_connect.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config()
-mongoose.connect(process.env.COMPASS_URI)
-.then(()=>{console.log("mongoose connected don't worry");})
+mongoose.connect(process.env.ATLAS_URI)
+.then(()=>{console.log("mongoose connected don't worry")})
 .catch(()=>{console.log("sorry bro please check once db not connected");})
 
 
